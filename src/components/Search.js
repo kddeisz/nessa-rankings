0e9fe620d0728e2018d77295c7112f0ff1cb5cd6
Kevin Deisz <kevin.deisz@gmail.com>
2019-03-20 18:25:49 -0400

    Abstract search icon

--- src/components/Search.js
+++ src/components/Search.js
@@ -5,6 +5,12 @@ import useField from "../utils/useField";
 
 import { t } from "./Trans";
 
+const Icon = ({ className, path }) => (
+  <svg width="22px" height="22px" viewBox="0 0 1024 1024" className={className}>
+    <path d={path} />
+  </svg>
+);
+
 const Search = ({ schools }) => {
   const [value, onChange] = useField();
   const filtered = useMemo(
@@ -21,9 +27,10 @@ const Search = ({ schools }) => {
   return (
     <>
       <label htmlFor="search" className="search--label">
-        <svg width="22px" height="22px" viewBox="0 0 1024 1024" className="search--icon">
-          <path d="M689 596c30-47 48-103 48-163 0-168-136-305-304-305-168 0-304 136-304 305s136 305 304 305c61 0 118-18 165-49l14-10 217 217 67-69-217-217 10-14zM603 262c45 45 70 106 70 170s-25 125-70 170c-45 45-106 70-170 70s-125-25-170-70c-45-45-70-106-70-170s25-125 70-170c45-45 106-70 170-70s125 25 170 70z" />
-        </svg>
+        <Icon
+          className="search--icon"
+          path="M689 596c30-47 48-103 48-163 0-168-136-305-304-305-168 0-304 136-304 305s136 305 304 305c61 0 118-18 165-49l14-10 217 217 67-69-217-217 10-14zM603 262c45 45 70 106 70 170s-25 125-70 170c-45 45-106 70-170 70s-125-25-170-70c-45-45-70-106-70-170s25-125 70-170c45-45 106-70 170-70s125 25 170 70z"
+        />
         <input
           className="search--input"
           aria-label={t("Search")}
